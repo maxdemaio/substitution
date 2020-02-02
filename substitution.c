0e0914cdedb589d0d238a128dbfdc08191941fe2
Max <44373521+maxwelldemaio@users.noreply.github.com>
2020-02-02 10:30:14 -0500

    Update substitution.c

--- substitution.c
+++ substitution.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <cs50.h>
+#include <ctype.h>
 
 // TO DO
 // Get key (command line argument pass into main, case insensitive, 26 long, alphabetic characters)
@@ -15,11 +16,43 @@ string encipher(string plaintext);
 // (number of command line arguments, string of command line arguments)
 int main(int argc, string argv[])
 {
+    string key = argv[1];
+    long keyLength = strlen(key);
+    int i;
+
+    printf("Key is: %s\n", key);
+    printf("Number Of Arguments Passed: %d\n", argc);
+    printf("Length of key is: %lu\n", keyLength);
+
+
     // Check key length
+    if (keyLength != 26)
+    {
+        printf("Key does not have a valid length!\n");
+        return (1);
+    }
+    printf("Key is valid length!\n");
+
+
     // Check for non-aplhabetic characters
+    for ( i = 0; i < keyLength; i++)
+    {
+        // If character is alphabetic, continue
+        if (isalpha(key[i]) != 0)
+        {
+            continue;
+        }
+        // Character error
+        else
+        {
+            printf("Key contains non-valid characters!\n");
+            return (1);
+        }
+    }
+    printf("Characters are valid!\n");
+
+
     // Check for repeated characters (case insensitive)
-    printf("Key is: %s\n",argv[1]);
-    printf("Number Of Arguments Passed: %d\n",argc);
 
     // Get plaintext input from user
     string plaintext = get_string("plain text: \n");
